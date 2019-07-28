6e27c9c89d8aa7d710f4ad1a831c6d159a160308
George Kirby <georgeskirby@btinternet.com>
2019-07-28 15:13:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,9 +3,6 @@ def roll_call_dwarves(names_array) # code an argument here
   names_array.each_with_index do |name, index|
     puts "#{index+1}. #{name}"
   end
-  
-  
-  
 end
 
 def summon_captain_planet# code an argument here
