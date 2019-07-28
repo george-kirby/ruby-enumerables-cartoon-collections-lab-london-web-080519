6e396df3801ab8cf4c0bdefaf4c6d8037f9f3da1
George Kirby <georgeskirby@btinternet.com>
2019-07-28 15:22:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ end
 
 def long_planeteer_calls(calls_array) # code an argument here
   # Your code here
-  calls_array.reduce {|memo, word| memo && word} 
+  calls_array.any? {|word| word.length > 4} 
 end
 
 def find_the_cheese# code an argument here
