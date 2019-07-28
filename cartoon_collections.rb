eea603be64fa7694c5323c5bde3fe31ba0d037fd
George Kirby <georgeskirby@btinternet.com>
2019-07-28 15:25:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(calls_array) # code an argument here
   calls_array.any? {|word| word.length > 4} 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(strings_array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
